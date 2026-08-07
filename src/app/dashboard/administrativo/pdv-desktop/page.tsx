4a2e2941e0d8521a5dfdcd1efefa668e1a6b5a95
jorgeestudos7-lang <jorgeestudos7@gmail.com>
2026-08-07 13:26:29 -0300

    feat: add PDV Desktop management route

---
+++ src/app/dashboard/administrativo/pdv-desktop/page.tsx
@@ -0,0 +1,18 @@
+import '../../[...slug]/module.css';
+import '../../[...slug]/advanced.css';
+import '../../[...slug]/pdv-device.css';
+import { AdvancedShell } from '../../[...slug]/advanced-shell';
+import { PdvDeviceWorkspace } from '../../[...slug]/pdv-device-workspace';
+import { pdvDeviceList } from '../../[...slug]/pdv-device-actions';
+import { erpLoad } from '../../[...slug]/actions';
+
+export default async function PdvDesktopPage() {
+  const [posRegisters, devices] = await Promise.all([erpLoad('pos_registers'), pdvDeviceList()]);
+  return <AdvancedShell
+    title="PDV Desktop / Agentes Windows"
+    subtitle="Pareie computadores de caixa com o ThorERP, monitore o agente e controle a sincronização das operações."
+    activePath="/dashboard/administrativo/pdv-desktop"
+  >
+    <PdvDeviceWorkspace posRegisters={posRegisters.data} initialDevices={devices.data} />
+  </AdvancedShell>;
+}
